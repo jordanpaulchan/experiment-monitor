af8665e8f5dee66f4bce0b1a7b8da71da012997d
jordanpaulchan <jordanpaulchan@gmail.com>
2018-06-16 23:33:00 -0700

    symatic layout foundation

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles/index.css";
+import "./index.css";
 import "./semantic/dist/semantic.min.css";
 
 import App from "./App";
